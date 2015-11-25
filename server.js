5dba6569610dbf75fe804b8f20ef0818d2c2730e
Niccolò Cometto <niccolo.cometto@gmail.com>
2015-11-25 11:42:59 +0100

    Update server.js
    codice temporaneo commentato , aggiunta la rimozione del file :)

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-var express = require('express');
+var express = require('express');    // inclusione modulo express per gestire la comunicazione verso il nostro server
 var app = express();
-var request = require('request');
-var multer=require('multer');
+var request = require('request');//inclusione del modulo request per gestire le richieste REST verso le varie piattaforme
+var multer=require('multer'); //inclusione del modulo multer per prendere le immagini dalle post ricevute sul server
 var upload=multer();
-var fs = require('fs')
+var fs = require('fs');//inclusione del modulo fs(file System) per gestire il salvataggio dei file immagine sul server per poi poterle riinviare
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+  res.send('Welcome to Aptors!!!!');
+}); //comando di test sul server , inviano una get alla root('/') del server restituisce il messaggio di benvenuto
 
 
-
+//Sezione di codice che si occupa di cosa accade se viene fatta una post sull'indirizzo di roott 
 app.post('/',  upload.single('datas'),function(req, res){
-	
+	// la variabile url specifica che stiamo andando l'indirizzo dove vogliamo fare la richiesta rest(nel nostro caso usiamo le api di keylemon)
+	// nella url ci autentichiamo presso il sito stesso per accedere alle API
 	var url='https://klws.keylemon.com/api/recognize/?user=jacopo1395&key=j83bSVhaKzkI1EeT7DGHeYAjwbIRhQhVXyxVKHEFhNoh2fU0PggVPL&groups=f2bcc191-f31f-467c-b5a5-56842b49dd7e';
+	//la variabile header ci serve perchè la richiesta che stiamo andando a fare richiede questo tipo di Header
 	var header={ 'Content-type':'multipart/form-data'
 	}
-	
+	//Nella post fatta al nostro server (dall'applicazione android ) viene inviato un file immagine che salviamo tramite fs
 	fs.writeFile(__dirname + '/temp/foto.jpg', req.file.buffer);
 	console.log(req.body);
 	console.log(req.file);
@@ -29,15 +31,21 @@ app.post('/',  upload.single('datas'),function(req, res){
 		  };
 		  console.log(formData);
 		  
-		  
+	// ora tramite il modulo request inviamo la post che ha come argomenti url e form data e ci restituirà
+	// il nome dell'attore nella foto
 	request.post({url:url, formData: formData}, function optionalCallback(err, httpResponse, body) {
 			  if (err) {
 				return console.error('upload failed:', err);
 			  }
 			  console.log('Upload successful!  Server responded with:', body);
 			  res.send(body);
-			  
+ //in seguito cancelliamo la foto dal nostro server			  
 			});
+	fs.unlink(_dirname+'/temp/foto.jpg',function(err){
+  if (err) throw err;
+  console.log('successfully deleted /tmp/hello');
+});)
+	
  
 });  
 
